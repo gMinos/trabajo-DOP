c645dc471dd43f45fc55c46d0c7d05ae2652c295
gMinos <gjosebelarmino@gmail.com>
2024-06-08 17:00:54 -0500

    mejorando el lagibilidad del codigo en pais/models.py, idioma/models.py

--- applications/idioma/models.py
+++ applications/idioma/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from applications.pais.models import Pais
 
+
 class Idioma(models.Model):
-    codigoIdioma = models.CharField('Codigo Idioma', max_length=10, unique=True)
+    codigoIdioma = models.CharField(
+        'Codigo Idioma', max_length=10, unique=True)
     idioma = models.CharField('Nombre Idioma', max_length=20, unique=True)
     idioma_pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
+
     def __str__(self) -> str:
         return self.codigoIdioma + ' - ' + self.idioma
