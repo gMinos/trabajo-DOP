ed2fed3a52de875a9e188f394d5610a8433de86b
gMinos <gjosebelarmino@gmail.com>
2024-06-08 16:08:35 -0500

    se ha terminado el modelo continente

--- applications/continente/admin.py
+++ applications/continente/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Continente
 
-# Register your models here.
+
+admin.site.register(Continente)
--- applications/continente/models.py
+++ applications/continente/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+# from applications.pais.models import Pais
 
 
 class Continente(models.Model):
     nombre_continente = models.CharField(
         'Nombre continente', max_length=20, unique=True)
+    descripcion_continente = models.CharField(
+        'Descripcion continente', max_length=50, unique=True, default='')
+    # relacion_pais = models.ManyToManyField(Pais)
+
+    def __str__(self):
+        return f'{self.id} - {self.nombre_continente} - {self.descripcion_continente}'
