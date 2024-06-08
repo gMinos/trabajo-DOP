9736a4f31ecae2b47a39f18e70901b7c979232cb
Natanael <reyesnatanael28@gmail.com>
2024-06-08 16:16:05 -0500

    clase idioma

--- applications/idioma/admin.py
+++ applications/idioma/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Idioma
 
 # Register your models here.
+admin.site.register(Idioma)
+
--- applications/idioma/models.py
+++ applications/idioma/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from applications.pais.models import Pais
 
-# Create your models here.
+class Idioma(models.Model):
+    codigoIdioma = models.CharField('Codigo Idioma', max_length=10, unique=True)
+    idioma = models.CharField('Nombre Idioma', max_length=20, unique=True)
+    idioma_pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return self.codigoIdioma + ' - ' + self.idioma
